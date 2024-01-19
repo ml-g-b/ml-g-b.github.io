770db0d6d7537740c2d00e7755a6804573f82561
ml-g-b <98955152+ml-g-b@users.noreply.github.com>
2024-01-19 18:57:21 +0100

    Update responsive.js

--- script/responsive.js
+++ script/responsive.js
@@ -1,1 +1,11 @@
-console.log("Successfully found")
+    const languages=document.querySelectorAll("language")
+    const techno=document.querySelector("#technologies")
+    languages.forEach(function(element){
+      var icon=document.createElement("img")
+      const lang=element.getAttribute("type")
+      icon.src="../logo/"+lang+".svg"
+      icon.alt=lang+" programming language icon"
+      icon.title=lang
+      element.remove()
+      techno.after(icon)
+    })
